1e534f0ad41356cd7ad09a08228c2e64b90c642d
Snehag Gupta <110154092+snehagupta05@users.noreply.github.com>
2023-07-06 21:44:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let move_speed = 4, grativy = 0.5;
-let bird = document.querySelector('.bird');
+let bird = document.querySelector('bird');
 let img = document.getElementById('bird-1');
 let sound_point = new Audio('point.mp3');
 let sound_die = new Audio('die.mp3');
